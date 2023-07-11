f94f787c5707622541b6bde5395f55448ab4b432
KanchiHaribabu <136108534+KanchiHaribabu@users.noreply.github.com>
2023-07-11 17:20:10 +0530

    Add files via upload

---
+++ Assignments/Assignment-6/P6/f.js
@@ -0,0 +1,12 @@
+const prompt =require("prompt-sync")();
+
+
+let numbers = [];
+let input = parseInt(prompt("Enter a number (enter 0 to stop):"));
+
+while (input !== 0) {
+    numbers.push(input);
+    input = parseInt(prompt("Enter a number (enter 0 to stop):"));
+}
+
+console.log(numbers);
