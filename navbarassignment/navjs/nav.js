5ecd5d06c25002b1a7d2752c9ff13ef3da376ea8
KanchiHaribabu <136108534+KanchiHaribabu@users.noreply.github.com>
2023-07-18 19:51:38 +0530

    Add files via upload

---
+++ navbarassignment/navjs/nav.js
@@ -0,0 +1,5 @@
+let favcolor = prompt("this is my fav colour");
+console.log("this is my fav colour is:"+ color);
+
+let favcolor = document.body.firstChild;
+firstchild.style.backgroundcolour="red";
